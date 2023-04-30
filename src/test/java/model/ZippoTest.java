b4d08ab5d80701ada7055f1f656e86fb657db4c9
ChatGTHB <cihat.kose@hotmail.com>
2023-04-30 16:01:32 +0300

    pathParamTest method updated

--- src/test/java/model/ZippoTest.java
+++ src/test/java/model/ZippoTest.java
@@ -126,4 +126,42 @@ public class ZippoTest {
     }
 
 
+    @Test
+    public void combinigTest() {
+
+        given()
+
+
+                .when()
+                .get("http://api.zippopotam.us/us/90210")
+
+
+                .then()
+                .log().body()    // Returning body json data, log().all()
+                .statusCode(200) // Is return code 200?
+                .body("places",hasSize(1)) // Is size "1" ?
+                .body("places.state",hasItem("California")) // Does the list in the given path have this item?
+                .body("places[0].'place name'",equalTo("Beverly Hills")) // Is the value in the given path equal to this?
+        ;
+    }
+
+    @Test
+    public void pathParamTest() {
+
+        given()
+                .pathParam("country","us")
+                .pathParam("zipCode",90210)
+                .log().uri()
+
+
+                .when()
+                .get("http://api.zippopotam.us/{country}/{zipCode}")
+
+
+                .then()
+                //.log().body()    // Returning body json data, log().all()
+                .statusCode(200) // Is return code 200?
+        ;
+    }
+
 }
