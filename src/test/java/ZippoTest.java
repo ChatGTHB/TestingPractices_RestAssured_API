d417d2c3dbd10c29849928246a9458c72a3d06ee
ChatGTHB <cihat.kose@hotmail.com>
2023-04-30 14:11:22 +0300

    checkCountryInResponseBody method has added

--- src/test/java/ZippoTest.java
+++ src/test/java/ZippoTest.java
@@ -2,6 +2,7 @@ import io.restassured.http.ContentType;
 import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
 
 public class ZippoTest {
     @Test
@@ -32,8 +33,8 @@ public class ZippoTest {
 
 
                 .then()
-                .log().body()    // returning body json data, log().all()
-                .statusCode(200) // is return code 200 ?
+                .log().body()    // Returning body json data, log().all()
+                .statusCode(200) // Is return code 200 ?
         ;
     }
 
@@ -48,9 +49,28 @@ public class ZippoTest {
 
 
                 .then()
-                .log().body()    // returning body json data, log().all()
-                .statusCode(200) // is return code 200?
-                .contentType(ContentType.JSON) // is the returned result JSON?
+                .log().body()    // Returning body json data, log().all()
+                .statusCode(200) // Is return code 200?
+                .contentType(ContentType.JSON) // Is the returned result JSON?
         ;
     }
+
+    @Test
+    public void checkCountryInResponseBody() {
+
+        given()
+
+
+                .when()
+                .get("http://api.zippopotam.us/us/90210")
+
+
+                .then()
+                .log().body()    // Returning body json data, log().all()
+                .statusCode(200) // Is return code 200?
+                .body("country",equalTo("United States")) // Is body's country variable equal to "United States"?
+        ;
+    }
+
+
 }
