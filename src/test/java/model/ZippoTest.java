2786eefa9e85dcc813ce3ab5afd167b9f1a37386
ChatGTHB <cihat.kose@hotmail.com>
2023-05-01 13:59:24 +0300

    extractingJsonPath2 method has added.

--- src/test/java/model/ZippoTest.java
+++ src/test/java/model/ZippoTest.java
@@ -265,10 +265,24 @@ public class ZippoTest {
     @Test
     public void extractingJsonPath() {
 
-        String countryName = given().when().get("http://api.zippopotam.us/us/90210").then().extract().path("country");
+        String countryName = given().when().get("http://api.zippopotam.us/us/90210").then().log().body().extract().path("country");
 
         System.out.println("countryName = " + countryName);
         Assert.assertEquals(countryName,"United States");
     }
 
+    @Test
+    public void extractingJsonPath2() {
+
+        String placeName = given()
+                .when()
+                .get("http://api.zippopotam.us/us/90210")
+
+                .then()
+                .log().body()
+                .extract().path("places[0].'place name'"); // places[0]['place name']
+
+        System.out.println("placeName = " + placeName);
+        Assert.assertEquals(placeName,"Beverly Hills");
+    }
 }
