ae7025d9e465588c7f60d44f5c353859c8c16b4d
ChatGTHB <cihat.kose@hotmail.com>
2023-05-05 10:31:20 +0300

    task2 solved

---
+++ src/test/java/Tasks.java
@@ -0,0 +1,44 @@
+
+import io.restassured.http.ContentType;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
+public class Tasks {
+
+
+
+
+
+
+
+
+
+
+    /**
+     * Task 2
+     * create a request to https://httpstat.us/203
+     * expect status 203
+     * expect content type TEXT
+     */
+
+    @Test
+    public void task2()
+    {
+        given()
+
+                .when()
+                .get("https://httpstat.us/203")
+
+                .then()
+                .log().all()
+                .statusCode(203)
+                .contentType(ContentType.TEXT)
+        ;
+    }
+
+
+
+}
