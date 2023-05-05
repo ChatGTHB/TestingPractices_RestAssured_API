d4274ad157cc69a7daaa1708a86930e38e454314
ChatGTHB <cihat.kose@hotmail.com>
2023-05-05 19:25:32 +0300

    PathAndJsonPath class has created

---
+++ src/test/java/PathAndJsonPath.java
@@ -0,0 +1,24 @@
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class PathAndJsonPath {
+    @Test
+    public void extractingPath() {
+
+        String postCode=
+
+        given()
+
+                .when()
+                .get("http://api.zippopotam.us/us/90210")
+
+                .then()
+                .log().body()
+                .extract().path("'post code'")
+        ;
+
+        System.out.println("postCode = " + postCode);
+
+    }
+}
