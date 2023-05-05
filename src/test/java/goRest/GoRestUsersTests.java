be5ab17a6c6f978eb1a4a32de7fbbc1d71d6f26b
ChatGTHB <cihat.kose@hotmail.com>
2023-05-05 13:36:24 +0300

    GoRestUsersTests class created.

---
+++ src/test/java/goRest/GoRestUsersTests.java
@@ -0,0 +1,52 @@
+package goRest;
+
+import io.restassured.http.ContentType;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class GoRestUsersTests {
+
+    @Test
+    public void createUser() {
+
+        /**
+         POST https://gorest.co.in/public/v2/users
+         "Authorization: Bearer 4b97b6d4f186b3272628a611715896f3ab8577ae2ea6ccb07b24ca2ae90d60fc"
+         {"name":"{{$randomFullName}}", "gender":"male", "email":"{{$randomEmail}}", "status":"active"}
+         */
+
+        int userID =
+
+                given()
+                        .header("Authorization", "Bearer 4b97b6d4f186b3272628a611715896f3ab8577ae2ea6ccb07b24ca2ae90d60fc")
+                        .contentType(ContentType.JSON) // Data to be sent is JSON
+                        .body("{\"name\":\"Kerem Yigit\", \"gender\":\"male\", \"email\":\"keremyigit@gmail.com\", \"status\":\"active\"}")
+                        .log().uri()
+                        .log().body()
+
+                        .when()
+                        .post("https://gorest.co.in/public/v2/users")
+
+                        .then()
+                        .statusCode(201)
+                        .contentType(ContentType.JSON)
+                        .extract().path("id");
+    }
+
+    @Test
+    public void getUserByID() {
+    }
+
+    @Test
+    public void updateUser() {
+    }
+
+    @Test
+    public void deleteUser() {
+    }
+
+    @Test
+    public void deleteUserNegative() {
+    }
+}
